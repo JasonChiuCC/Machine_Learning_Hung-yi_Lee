173f27a1478bee609bc05df200e4745d32f148c7
JasonChiuCC <jasonchiucc@gmail.com>
2017-12-26 13:28:12 +0800

    Update README.py

--- README.py
+++ README.py
@@ -10,3 +10,26 @@ for i, elem in enumerate(rList):
     else:
       resDict.update( { elem: [idx,1] } )
       idx+=1
+
+        
+# -*- coding: utf-8 -*-
+import math
+from PIL import Image
+
+im = Image.open( "westbrook.png" )
+rgb_im = im.convert('RGB')
+pixels = rgb_im.load() # create the pixel map
+
+# Get Size
+width, height = im.size
+print(width, height)
+
+
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
+        r, g, b = rgb_im.getpixel((i, j))
+        pixels[i,j] = ( math.floor(r/2), math.floor(g/2), math.floor(b/2))
+
+rgb_im.show()
+
+        
